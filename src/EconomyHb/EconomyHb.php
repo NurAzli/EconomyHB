2095bf1b439e3ea96df3def03d75ce8fcd2c37d1
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-01-02 13:46:21 +0700

    Update EconomyHb.php

--- src/EconomyHb/EconomyHb.php
+++ src/EconomyHb/EconomyHb.php
@@ -4,6 +4,9 @@ namespace EconomyHb;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 
 class EconomyHb extends PluginBase {
 
@@ -18,6 +21,9 @@ class EconomyHb extends PluginBase {
 
         // Inisialisasi konfigurasi
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+
+        // Register command
+        $this->getServer()->getCommandMap()->register("economyhb", new CheckEconomyCommand($this));
     }
 
     public function giveSalary($player, $job, $amount) {
@@ -31,5 +37,26 @@ class EconomyHb extends PluginBase {
     public function getPlayerEconomy($player) {
         $playerName = $player->getName();
         return $this->config->getNested("economy.$playerName", 0);
+    }
+}
+
+class CheckEconomyCommand extends Command {
+
+    private $plugin;
+
+    public function __construct(EconomyHb $plugin) {
+        parent::__construct("checkeconomy", "Check your current economy", null, ["ce"]);
+        $this->setPermission("economyhb.command.checkeconomy");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if ($sender instanceof Player) {
+            $this->plugin->checkPlayerEconomy($sender);
+            return true;
+        } else {
+            $sender->sendMessage("This command can only be used in-game.");
+            return false;
+        }
     }
 }
